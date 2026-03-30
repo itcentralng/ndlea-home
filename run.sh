b4236a129d229c166126ce6b84257bbb01955593
Nasir Mustapha <teboy.com@gmail.com>
2026-03-30 16:38:58 +0100

    Remove run.sh script for launching the app in Edge kiosk mode

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-"/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge" \
-  --kiosk "file://$DIR/index.html" \
-  --edge-kiosk-type=fullscreen \
-  --no-first-run \
-  --disable-features=msEdgeEnterpriseModePolicies
